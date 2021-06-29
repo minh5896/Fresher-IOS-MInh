6d4e7dbd1e7a474ae1e2c4133e7c0e2dcdf2564e
minh5896 <83936069+minh5896@users.noreply.github.com>
2021-06-29 10:48:05 +0700

    Add files via upload

---
+++ Basicquestion.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+var NUMBER = [Int]()
+for i in 0...17 {
+    NUMBER.append(i)
+    print(NUMBER)
+}
+var cau1 = "What is Swift programing language ?"
+print("Swift is a  programming language for iOS, macOS, watchOS, and tvOS app development. Nonetheless, many parts of Swift will be familiar from your experience of developing in C and Objective-C.")
+var cau2 = "Why is Swift a type-safe language?"
+print("Swift lets us create variables as strings and integers, but also many other types of data too. When you create a variable Swift can figure out what type the variable is based on what kind of data you assign to it, and from then on that variable will always have that one specific type.")
+var cau3 = "What is Constants and Variables ? How to declare Constants and Variables in Swift ?"
+print(" constant is a value, and it cannot be changed.Variables are the variables and can be changed.")
+print("constants use the let keyword.constant:-A constant does not change its value over time.Constants are usually written in numbers.Constants usually represent the known values in an equation, expression or in line of programming.Constants are used in computer programming.")
+print(" variable use the var keyword.Variables:A variable, on the other hand, changes its value dependent on the equation.Variables are specially written in letters or symbols.Variables, on the other hand, represent  the unknown values.Variables also have its uses in computer programming and applications.")
